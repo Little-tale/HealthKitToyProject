c701c7e7b7387b9b164645d25652bd4826073d67
Kim JaeHyung <you3192001@icloud.com>
2024-07-17 14:58:57 +0900

    Initial Commit

---
+++ HealthKitToyProject/HealthKitToyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitToyProjectApp.swift
+//  HealthKitToyProject
+//
+//  Created by Jae hyung Kim on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitToyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
