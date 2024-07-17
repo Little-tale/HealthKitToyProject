5685442ac7e29e5b4310e41cfa556828201215e7
Kim JaeHyung <you3192001@icloud.com>
2024-07-17 15:09:36 +0900

    Fetch: 헬스킷(폰) 으로 가능한것 들 정리

--- HealthKitToyProject/ContentView.swift
+++ HealthKitToyProject/ContentView.swift
@@ -6,8 +6,32 @@
 //
 
 import SwiftUI
-
+/*
+ 헬스 킷 (p: Phone, W: 웨어러블 기기)
+    기본 앱 가져오는 데이터
+        -w 심박수 heartRate
+        -w 휴식기 심박수 restingHeartRate
+        -w 심박 변이 walkingHeartRateAcerage
+        -w 걷기 심박수 평균 heartRateVariabilitySDNN
+        ... 생략 
+        워치가 없는관계로 폰으로 가능한것만 정리
+        -p 수면
+        
+        -p 보행 속도 walkingSpeed
+        -p 보행 비대칭성 walkingAsymmetryPercentage
+        -p 이중 지지 시간 walkingDoubleSupportPercentage
+        -p 보행 보폭 walkingStepLength
+        -p 보행 안정성 appleWalkingSteadiness
+        ...
+        -p 걷기 + 달리기 거리 distanceWalkingRunning
+        -p 활동 에너지 activeEnergyBurned
+        -p 오른 층계 flightsClimbed
+        -p 걸음 stepCount
+        -p 휴식 에너지 basalEnergyBurned
+ */
 struct ContentView: View {
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
