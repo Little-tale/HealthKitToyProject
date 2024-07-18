408a867e6c31e1bb5376178ca57ac6c713c32fb9
Kim JaeHyung <you3192001@icloud.com>
2024-07-18 17:27:42 +0900

    Fetch: 사용자의 몸무게 정보 가져오기 업데이트

--- HealthKitToyProject/HealthManager.swift
+++ HealthKitToyProject/HealthManager.swift
@@ -18,8 +18,12 @@ final class HealthManager: ObservableObject {
     init() {
         //  숫자 값 데이터 유형
         let steps = HKQuantityType(.stepCount)
+        let bodyInfo = HKQuantityType(.bodyMass)
         
-        let healthTypes: Set = [steps]
+        let healthTypes: Set = [
+            steps,
+            bodyInfo
+        ]
         
         Task {
             do {
@@ -29,12 +33,25 @@ final class HealthManager: ObservableObject {
             }
         }
     }
-       
+    /// 필요한 모든 정보를 방출
     func getInfo() {
+        let stepQuery = requestToSteps()
+        let userWeightQuery = reqeustUserWeight()
+        healthStore.execute(stepQuery)
+        healthStore.execute(userWeightQuery)
+    }
+    
+}
+
+// MARK: 요청 로직
+extension HealthManager {
+    /// 걸음수 요청
+    private func requestToSteps() -> HKStatisticsQuery {
         let stpes = HKQuantityType(.stepCount)
         /// 데이터 샘플을 나타내는 데 사용되는 클래스 의 추상 하위 클래스
         let predicated = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
-        let query = HKStatisticsQuery(quantityType: stpes, quantitySamplePredicate: predicated) { _, result, error in
+        
+        return HKStatisticsQuery(quantityType: stpes, quantitySamplePredicate: predicated) { _, result, error in
             guard let result = result, error == nil else {
                 print("문제발생")
                 return
@@ -45,6 +62,7 @@ final class HealthManager: ObservableObject {
             }
             
             let stepCount = quantity.doubleValue(for: .count())
+            
             if let stepString = stepCount.formatToString() {
                 let model = HealthInfo(
                     title: HealthInfoType.steps.title,
@@ -53,29 +71,54 @@ final class HealthManager: ObservableObject {
                     rightImage: HealthInfoType.steps.subImage,
                     sort: HealthInfoType.steps.sort
                 )
-                
                 DispatchQueue.main.async { [unowned self] in
                     healthInfo[HealthInfoType.steps.rawValue] = model
                 }
-                
             } else {
                 return
             }
         }
+    }
+    
+    private func reqeustUserWeight() -> HKSampleQuery {
+        let bodyMass = HKQuantityType(.bodyMass)
         
+        let body = HKSampleQuery(sampleType: bodyMass, predicate: nil, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { query, results, error in
+            let weight = results?.last as? HKQuantitySample
+            
+            guard let weight = weight else { return }
+            let weightDouble = weight.quantity.doubleValue(for: .gramUnit(with: .kilo))
+            let str = (String(format: "%.2f", weightDouble) + "KG")
+            
+            let model = HealthInfo(
+                title: HealthInfoType.userWeight.title,
+                subTitle:  HealthInfoType.userWeight.subImage,
+                amout: str,
+                rightImage:  HealthInfoType.userWeight.subImage,
+                sort:  HealthInfoType.userWeight.sort
+            )
+            
+            DispatchQueue.main.async { [unowned self] in
+                healthInfo[HealthInfoType.userWeight.rawValue] = model
+            }
+        }
         
-        healthStore.execute(query)
+        return body
     }
     
 }
 
 enum HealthInfoType: String, Hashable {
     case steps
+    case userWeight
+    
     
     var title: String {
         switch self {
         case .steps:
             return "걸음수"
+        case .userWeight:
+            return "몸무게"
         }
     }
     
@@ -83,6 +126,8 @@ enum HealthInfoType: String, Hashable {
         switch self {
         case .steps:
             return "최종 목표: 10000"
+        case .userWeight:
+            return ""
         }
     }
     
@@ -90,6 +135,8 @@ enum HealthInfoType: String, Hashable {
         switch self {
         case .steps:
             return "figure.walk"
+        case .userWeight:
+            return "figure.stand"
         }
     }
     
@@ -97,6 +144,8 @@ enum HealthInfoType: String, Hashable {
         switch self {
         case .steps:
             return 0
+        case .userWeight:
+            return 1
         }
     }
 }
