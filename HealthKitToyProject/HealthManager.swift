7acaf82d4635d1e471b159f196638251d52fa34f
Kim JaeHyung <you3192001@icloud.com>
2024-07-20 19:24:41 +0900

    Fetch: 수면 시간 업데이트

--- HealthKitToyProject/HealthManager.swift
+++ HealthKitToyProject/HealthManager.swift
@@ -40,8 +40,11 @@ final class HealthManager: ObservableObject {
     func getInfo() {
         let stepQuery = requestToSteps()
         let userWeightQuery = reqeustUserWeight()
+        let sleepQuery = reqeustUserSleepData()
+        
         healthStore.execute(stepQuery)
         healthStore.execute(userWeightQuery)
+        healthStore.execute(sleepQuery)
     }
     
 }
@@ -109,12 +112,75 @@ extension HealthManager {
         return body
     }
     
+    
+    func reqeustUserSleepData() ->  HKSampleQuery {
+        let sleepType = HKCategoryType(.sleepAnalysis)
+        /// HKCategoryValueSleepAnalysis  @available(iOS 16.0, *)
+        let sleepData = HKCategoryValueSleepAnalysis.asleepDeep.rawValue
+        /// HKCategoryType 예전엔 직접 샘플 필터를 통해 각 수면 데이터를 가져와야함.
+//        let beforeDataType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+        
+        // 날짜 범위 세팅
+        let calendar = Calendar.current
+        let startDate = calendar.startOfDay(for: Date())
+        let endDate = calendar.date(byAdding: .day, value: 1, to: startDate)!
+        
+//        //  NSPredicate
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
+        
+        let body = HKSampleQuery(sampleType: sleepType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { query, results, error in
+            
+            guard let results = results as? [HKCategorySample] else {
+                print("수면 데이터 에러가 발생했습니다: \(String(describing: error))")
+                return
+            }
+            
+            let totalSleepTime = results
+                .filter { sample in
+                    return sample.value == HKCategoryValueSleepAnalysis.asleepUnspecified.rawValue ||
+                    sample.value == HKCategoryValueSleepAnalysis.asleepCore.rawValue ||
+                    sample.value == HKCategoryValueSleepAnalysis.asleepDeep.rawValue ||
+                    sample.value == HKCategoryValueSleepAnalysis.asleepREM.rawValue
+                }
+                .reduce(0){ total, sample in // O(*n*)
+                    return total + sample.endDate.timeIntervalSince(sample.startDate)
+                }
+            // 총 수면 시간 계산 및 출력
+            let hours = totalSleepTime / 3600
+            let minutes = (totalSleepTime.truncatingRemainder(dividingBy: 3600)) / 60
+            let sleepTimeString = String(format: "%.0f시간 %.0f분", hours, minutes)
+            
+            let model = HealthInfo(
+                title: HealthInfoType.sleep.title,
+                subTitle:  HealthInfoType.sleep.sub,
+                amout: sleepTimeString,
+                rightImage:  HealthInfoType.sleep.subImage,
+                sort:  HealthInfoType.sleep.sort
+            )
+            
+            DispatchQueue.main.async { [unowned self] in
+                healthInfo[HealthInfoType.sleep.rawValue] = model
+            }
+        }
+    
+        return body
+    }
+    
 }
+
+/*
+ let sampleData = HKCategorySample(
+     type: sleepType,
+     value: sleepData,
+     start: startDate,
+     end: endDate
+ )
+ */
 
 enum HealthInfoType: String, Hashable {
     case steps
     case userWeight
-    
+    case sleep
     
     var title: String {
         switch self {
@@ -122,6 +188,8 @@ enum HealthInfoType: String, Hashable {
             return "걸음수"
         case .userWeight:
             return "몸무게"
+        case .sleep:
+            return "수면 시간"
         }
     }
     
@@ -131,6 +199,8 @@ enum HealthInfoType: String, Hashable {
             return "최종 목표: 10000"
         case .userWeight:
             return ""
+        case .sleep:
+            return "권장 수면: 8시간"
         }
     }
     
@@ -140,6 +210,8 @@ enum HealthInfoType: String, Hashable {
             return "figure.walk"
         case .userWeight:
             return "figure.stand"
+        case .sleep:
+            return "bed.double.fill"
         }
     }
     
@@ -149,6 +221,8 @@ enum HealthInfoType: String, Hashable {
             return 0
         case .userWeight:
             return 1
+        case .sleep:
+            return 2
         }
     }
 }
